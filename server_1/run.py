24f2c6de53003f108b00a1087c544561d6dc6d8f
jerry <jerryochieng178@gmail.com>
2025-01-14 19:51:07 +0300

    Python Server Changes

--- server_1/run.py
+++ server_1/run.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from app.util.talos_vector_database_engine import initialize
+# from app.util.talos_vector_database_engine import initialize
 from app import app
 if __name__ == '__main__':
     # Load environment variables from .env file
